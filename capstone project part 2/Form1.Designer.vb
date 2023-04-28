ee3a4c2684be63a44d11c912b4a2c755adc148a7
zdavis1117 <zdavis1117@W10-1SM2CS3>
2023-04-28 09:28:32 -0500

    4.28 (9:28)

--- capstone project part 2/Form1.Designer.vb
+++ capstone project part 2/Form1.Designer.vb
@@ -22,18 +22,43 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.components = New System.ComponentModel.Container()
+        Me.PicWheel = New System.Windows.Forms.PictureBox()
+        Me.btnSpin = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PicWheel, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'PictureBox1
+        'PicWheel
         '
-        Me.PictureBox1.Image = Global.capstone_project_part_2.My.Resources.Resources.WOF2
-        Me.PictureBox1.Location = New System.Drawing.Point(190, 83)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(902, 517)
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
+        Me.PicWheel.Image = Global.capstone_project_part_2.My.Resources.Resources.WOF2
+        Me.PicWheel.Location = New System.Drawing.Point(165, 51)
+        Me.PicWheel.Name = "PicWheel"
+        Me.PicWheel.Size = New System.Drawing.Size(938, 517)
+        Me.PicWheel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PicWheel.TabIndex = 0
+        Me.PicWheel.TabStop = False
+        '
+        'btnSpin
+        '
+        Me.btnSpin.Font = New System.Drawing.Font("Ravie", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSpin.Location = New System.Drawing.Point(224, 612)
+        Me.btnSpin.Name = "btnSpin"
+        Me.btnSpin.Size = New System.Drawing.Size(318, 114)
+        Me.btnSpin.TabIndex = 1
+        Me.btnSpin.Text = "Spin"
+        Me.btnSpin.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Ravie", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(714, 612)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(318, 113)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -41,13 +66,18 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
         Me.ClientSize = New System.Drawing.Size(1263, 748)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSpin)
+        Me.Controls.Add(Me.PicWheel)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicWheel, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PicWheel As PictureBox
+    Friend WithEvents btnSpin As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Timer1 As Timer
 End Class
--- capstone project part 2/Form1.vb
+++ capstone project part 2/Form1.vb
@@ -1,3 +1,85 @@
 ﻿Public Class Form1
 
+    Dim intval As Integer = 0
+    Private Sub btnSpin_Click(sender As Object, e As EventArgs) Handles btnSpin.Click
+
+        intval += 1
+        If intval = 1 Then
+            PicWheel.Image = My.Resources.WOF2
+
+        ElseIf intval = 2 Then
+            PicWheel.Image = My.Resources.WOF3
+
+        ElseIf intval = 3 Then
+            PicWheel.Image = My.Resources.WOF4
+
+        ElseIf intval = 4 Then
+            PicWheel.Image = My.Resources.WOF5
+
+        ElseIf Intval = 5 Then
+            PicWheel.Image = My.Resources.WOF6
+
+        ElseIf intval = 6 Then
+            PicWheel.Image = My.Resources.WOF7
+
+        ElseIf intval = 7 Then
+            PicWheel.Image = My.Resources.WOF8
+
+        ElseIf intval = 8 Then
+            PicWheel.Image = My.Resources.wof9
+
+        ElseIf intval = 9 Then
+            PicWheel.Image = My.Resources.wof10
+
+        ElseIf intval = 10 Then
+            PicWheel.Image = My.Resources.wof11
+
+        ElseIf intval = 11 Then
+            PicWheel.Image = My.Resources.wof12
+
+        ElseIf intval = 12 Then
+            PicWheel.Image = My.Resources.wof13
+
+        ElseIf intval = 13 Then
+            PicWheel.Image = My.Resources.wof14
+
+        ElseIf intval = 14 Then
+            PicWheel.Image = My.Resources.wof15
+
+        ElseIf intval = 15 Then
+            PicWheel.Image = My.Resources.wof16
+
+        ElseIf intval = 16 Then
+            PicWheel.Image = My.Resources.wof17
+
+        ElseIf intval = 17 Then
+            PicWheel.Image = My.Resources.wof18
+
+        ElseIf intval = 18 Then
+            PicWheel.Image = My.Resources.wof19
+
+        ElseIf intval = 19 Then
+            PicWheel.Image = My.Resources.WOF2
+
+        ElseIf intval = 20 Then
+            PicWheel.Image = My.Resources.wof21
+
+        ElseIf intval = 21 Then
+            PicWheel.Image = My.Resources.wof22
+
+        ElseIf intval = 22 Then
+            PicWheel.Image = My.Resources.wof23
+
+        ElseIf intval = 23 Then
+            PicWheel.Image = My.Resources.wof24
+
+        ElseIf intval = 24 Then
+            PicWheel.Image = My.Resources.wof25
+
+        Else
+            intval = 0
+
+
+
+    End Sub
 End Class
